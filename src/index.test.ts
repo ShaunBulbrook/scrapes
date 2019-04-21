3d6ca63b4079d7a1f206330c127490f996faad3d
Shaun Bulbrook <shaun.bulbrook@mirumagency.com>
2019-04-21 16:24:21 +0100

    Added functionality to rip hyperlink refs from markup

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
-import { getPage, scrapeSelection } from "./index";
-import { markup } from "./stubs/markup";
+import { getPage, scrapeSelection, scrapeLinks } from "./index";
+import { markup, hackerNewsMarkup } from "./stubs/markup";
 
 const fetch = jest.fn();
 fetch.mockReturnValue(markup);
@@ -27,5 +27,22 @@ describe("scrapeSelection", () => {
 		expect(
 			scrapeSelection(markup, `a[href="http://www.iana.org/domains/example"]`)
 		).toBe("More information...");
+	});
+});
+
+describe("scrapeLinks", () => {
+	it("should throw an error if no links are found", () => {
+		function scrapeLinksFromNothing() {
+			scrapeLinks("");
+		}
+		expect(scrapeLinksFromNothing).toThrowError("No links could be found.");
+	});
+	it("should return a link on a page", () => {
+		expect(scrapeLinks(markup)[0]).toBe("http://www.iana.org/domains/example");
+	});
+	it("should return different links", () => {
+		expect(scrapeLinks(hackerNewsMarkup)).toContain(
+			"http://www.antenna-theory.com/"
+		);
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,29 @@ export const getPage = async (location: string): Promise<string> => {
 
 /**
  * Takes markup and a query selector. Returns the text under elements that match the selector
- * @param markup html string
+ * @param markup string of html
  * @param selector query selector to rip text from
  */
 export const scrapeSelection = (markup: string, selector: string): string => {
 	const $: any = cheerio.load(markup);
 	return $(selector).text() || "No results found.";
+};
+
+/**
+ * Takes markup and returns an array of all links of the page.
+ * @param markup string of html
+ */
+export const scrapeLinks = (markup: string): string[] => {
+	const $: any = cheerio.load(markup);
+	const links: string[] = [];
+
+	$("a[href]").each((index: number, element: any) =>
+		links.push($(element).attr("href"))
+	);
+
+	if (links.length > 0) {
+		return links;
+	} else {
+		throw "No links could be found.";
+	}
 };
